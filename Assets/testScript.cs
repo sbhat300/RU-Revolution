4a6c9b7decd60a9542ede062d9131bfe1d7e6bde
sbhat300 <87624722+sbhat300@users.noreply.github.com>
2024-09-22 17:48:05 -0400

    circle

--- Assets/testScript.cs
+++ Assets/testScript.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class testScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.D)) rb.AddForce(new Vector2(5, 0));
     }
 }
